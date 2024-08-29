348298bfd489f5bdfce93c7dad45876006e34079
ErfanMelon <ErfanMelon@outlook.com>
2024-08-29 17:23:30 +0330

    create board and project repositories

---
+++ src/PhoenixTask.Domain/Projects/IBoardRepository.cs
@@ -0,0 +1,12 @@
+﻿using PhoenixTask.Domain.Abstractions.Maybe;
+
+namespace PhoenixTask.Domain.Projects;
+
+public interface IBoardRepository
+{
+    public void Insert(Board board);
+    public Task<IEnumerable<Board>> GetAllBoardsByProjectIdAsync(Guid projectId);
+    public Task<Maybe<Board>> GetBoardByIdAsync(Guid boardId);
+    public void Update(Board board);
+    public void Remove(Board board);
+}
---
+++ src/PhoenixTask.Domain/Projects/IProjectRepository.cs
@@ -0,0 +1,12 @@
+﻿using PhoenixTask.Domain.Abstractions.Maybe;
+
+namespace PhoenixTask.Domain.Projects;
+
+public interface IProjectRepository
+{
+    public void Insert(Project project);
+    public Task<IEnumerable<Project>> GetAllProjectsByWorkSpaceIdAsync(Guid workSpaceId);
+    public Task<Maybe<Project>> GetProjectByIdAsync(Guid projectId);
+    public void Update(Project project);
+    public void Remove(Project project);
+}
