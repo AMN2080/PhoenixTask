f0a10e016f4ecae2373ac643baeb65dfdfe85990
Alireza Mohamadzadeh <alireza3205@gmail.com>
2024-09-03 19:03:19 +0330

    edit Heading on UI

--- src/components/modules/ui/Heading.tsx
+++ src/components/modules/ui/Heading.tsx
@@ -2,9 +2,9 @@ import { createElement } from "react";
 import { fontWeight } from "@/logic/SharedComponentStyles/sharedStyles";
 
 const TextAlign = {
+  right: "text-right",
   center: "text-center",
   left: "text-left",
-  right: "text-right",
 };
 
 const Size = {
@@ -27,7 +27,6 @@ interface Props {
 const Heading: React.FC<Props> = ({
   as,
   children,
-  colorVariant = "primary",
   size = "XS",
   weight = "800",
   align = "right",
@@ -36,7 +35,7 @@ const Heading: React.FC<Props> = ({
   return createElement(
     as,
     {
-      className: `font-IranYekan ${Size[size]} ${fontWeight[weight]} ${className} ${TextAlign[align]}`,
+      className: `font-IranYekan text-neutral-content ${Size[size]} ${fontWeight[weight]} ${TextAlign[align]} ${className}`,
     },
     children,
   );
