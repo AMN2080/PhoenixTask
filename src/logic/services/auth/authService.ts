3ea5b9588bbba913d449cbfb076bcec08078f263
Alireza Mohamadzadeh <alireza3205@gmail.com>
2024-09-09 21:59:43 +0330

    fix types in authService

--- src/logic/services/auth/authService.ts
+++ src/logic/services/auth/authService.ts
@@ -1,16 +1,18 @@
 import axios from "axios";
-import { FieldValues } from "../../../pages/auth/Register";
+import { registerType } from "@/logic/schemas/registerSchema";
+import { loginType } from "@/logic/schemas/loginSchema";
+import { forgotPasswordType } from "@/logic/schemas/forgotPasswordSchema";
 
 const API_URL = "https://quera-task-server.vercel.app/api/auth/";
 
 // Regiter user
-const register = async (userData: FieldValues) => {
+const register = async (userData: registerType) => {
   const response = await axios.post(API_URL + "register", userData);
   return response.data;
 };
 
 // Login user
-const login = async (userData: FieldValues) => {
+const login = async (userData: loginType) => {
   const response = await axios.post(API_URL + "login", userData);
 
   if (response.data) {
@@ -19,18 +21,18 @@ const login = async (userData: FieldValues) => {
       JSON.stringify({
         accessToken: response.data.data.accessToken,
         refreshToken: response.data.data.refreshToken,
-      })
+      }),
     );
     localStorage.setItem(
       "user",
-      JSON.stringify(response.data.data.toBeSendUserData)
+      JSON.stringify(response.data.data.toBeSendUserData),
     );
   }
   return response.data;
 };
 
 // Forgot password
-const forgot = async (userEmail: FieldValues) => {
+const forgot = async (userEmail: forgotPasswordType) => {
   const response = await axios.post(API_URL + "forget-password", userEmail);
 
   return response.data;
