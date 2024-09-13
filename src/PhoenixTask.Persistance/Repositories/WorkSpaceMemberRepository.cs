dd62c1945dcd3dc02e2f71e839327d1bb17968f7
Erfan Memarpoor <ErfanMelon@outlook.com>
2024-09-13 18:56:05 +0330

    implement workspacememberrepository

--- src/PhoenixTask.Persistance/Repositories/WorkSpaceMemberRepository.cs
+++ src/PhoenixTask.Persistance/Repositories/WorkSpaceMemberRepository.cs
@@ -1,34 +1,26 @@
-﻿using PhoenixTask.Domain.Abstractions.Maybe;
+﻿using Microsoft.EntityFrameworkCore;
+using PhoenixTask.Application.Abstractions.Data;
+using PhoenixTask.Domain.Abstractions.Maybe;
 using PhoenixTask.Domain.Authorities;
 using PhoenixTask.Domain.Users;
 using PhoenixTask.Domain.Workspaces;
 
 namespace PhoenixTask.Persistance.Repositories;
 
-internal class WorkSpaceMemberRepository : IWorkSpaceMemberRepository
+internal class WorkSpaceMemberRepository(IDbContext dbContext) : GenericRepository<WorkSpaceMember>(dbContext), IWorkSpaceMemberRepository
 {
-    public Task<Maybe<WorkSpaceMember>> GetMemberByIdAsync(Guid workSpaceId, Guid memberId)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<Maybe<WorkSpaceMember>> GetMemberByIdAsync(Guid workSpaceId, Guid memberId)
+        => Maybe<WorkSpaceMember>.From(await DbContext.Set<WorkSpaceMember>()
+            .SingleOrDefaultAsync(x => x.UserId == memberId && x.WorkSpaceId == workSpaceId));
 
-    public Task<IEnumerable<WorkSpaceMember>> GetMembersByIdAsync(Guid workSpaceId)
+    public async Task<IEnumerable<WorkSpaceMember>> GetMembersByIdAsync(Guid workSpaceId)
+        => DbContext.Set<WorkSpaceMember>().Where(x => x.WorkSpaceId == workSpaceId);
+    public async Task<bool> UserHasRoleAsync(User user, WorkSpace workSpace, Role role)
     {
-        throw new NotImplementedException();
-    }
-
-    public void Insert(WorkSpaceMember workSpaceMember)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Remove(WorkSpaceMember workSpaceMember)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<bool> UserHasRoleAsync(User user, WorkSpace workSpace, Role role)
-    {
-        throw new NotImplementedException();
+        return await DbContext.Set<WorkSpaceMember>()
+            .Include(x => x.Roles)
+            .Where(x => x.UserId == user.Id && x.WorkSpaceId == workSpace.Id)
+            .SelectMany(x => x.Roles)
+            .AnyAsync(x => x == role);
     }
 }
