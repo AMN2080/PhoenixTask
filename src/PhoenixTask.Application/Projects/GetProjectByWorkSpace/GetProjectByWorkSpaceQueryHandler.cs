582d2942468f1c3e6e93eb10eaeba6f8583dfa15
ErfanMelon <ErfanMelon@outlook.com>
2024-09-26 17:33:47 +0330

    feat: Add user permission check for getting projects by workspace

--- src/PhoenixTask.Application/Projects/GetProjectByWorkSpace/GetProjectByWorkSpaceQueryHandler.cs
+++ src/PhoenixTask.Application/Projects/GetProjectByWorkSpace/GetProjectByWorkSpaceQueryHandler.cs
@@ -1,5 +1,7 @@
-﻿using PhoenixTask.Application.Abstractions.Authentication;
+﻿using MediatR;
+using PhoenixTask.Application.Abstractions.Authentication;
 using PhoenixTask.Application.Abstractions.Messaging;
+using PhoenixTask.Application.WorkSpaces.CheckPermission;
 using PhoenixTask.Contracts.Projects;
 using PhoenixTask.Domain.Projects;
 using PhoenixTask.Domain.Workspaces;
@@ -9,12 +11,13 @@ namespace PhoenixTask.Application.Projects.GetProjectByWorkSpace;
 internal sealed class GetProjectByWorkSpaceQueryHandler(
     IUserIdentifierProvider userIdentifierProvider,
     IWorkSpaceRepository workSpaceRepository,
-    IProjectRepository projectRepository) : IQueryHandler<GetProjectByWorkSpaceQuery, IEnumerable<ProjectResult>>
+    IProjectRepository projectRepository,
+    ISender sender) : IQueryHandler<GetProjectByWorkSpaceQuery, IEnumerable<ProjectResult>>
 {
     private readonly IUserIdentifierProvider _userIdentifierProvider = userIdentifierProvider;
     private readonly IWorkSpaceRepository _workSpaceRepository = workSpaceRepository;
     private readonly IProjectRepository _projectRepository = projectRepository;
-
+    private readonly ISender _sender =sender;
     public async Task<IEnumerable<ProjectResult>> Handle(GetProjectByWorkSpaceQuery request, CancellationToken cancellationToken)
     {
         var maybeWorkSpace = await _workSpaceRepository.GetByIdAsync(request.WorkSpaceId);
@@ -26,10 +29,7 @@ internal sealed class GetProjectByWorkSpaceQueryHandler(
 
         #region UserPermitToGetProjects
 
-        if (maybeWorkSpace.Value.OwnerId != _userIdentifierProvider.UserId)
-        {
-            return [];
-        }
+        var hasAccess = await _sender.Send(new HasWorkSpacePermissionCommand(maybeWorkSpace.Value.Id, Domain.Authorities.PermissionType.ReadProject));
         #endregion
 
         var projects = await _projectRepository.GetAllProjectsByWorkSpaceIdAsync(request.WorkSpaceId);
