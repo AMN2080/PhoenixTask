a3e76847c0d1f96593e62bf81fe2a8a208827baf
ErfanMelon <ErfanMelon@outlook.com>
2024-09-27 11:29:35 +0330

    feat: Add Task entity properties and methods

---
+++ src/PhoenixTask.Domain/Tasks/DomainEvents/TaskUpdatedDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using PhoenixTask.Domain.Abstractions.Events;
+
+namespace PhoenixTask.Domain.Tasks.DomainEvents;
+
+public sealed class TaskUpdatedDomainEvent : IDomainEvent
+{
+    internal TaskUpdatedDomainEvent(Task task) => Task = task;
+    public Task Task { get; }
+}
---
+++ src/PhoenixTask.Domain/Tasks/Task.cs
@@ -0,0 +1,60 @@
+﻿using PhoenixTask.Domain.Abstractions;
+using PhoenixTask.Domain.Abstractions.Guards;
+using PhoenixTask.Domain.Abstractions.Primitives;
+using PhoenixTask.Domain.Tasks.DomainEvents;
+using PhoenixTask.Domain.Users;
+using PhoenixTask.Domain.Workspaces;
+
+namespace PhoenixTask.Domain.Tasks;
+
+public sealed class Task : AggregateRoot, IAuditableEntity, ISoftDeletableEntity
+{
+    private Task(Name name,User user, string description, DateTime deadLine, int priority, int order)
+        :base(Guid.NewGuid())
+    {
+        Ensure.NotEmpty(name, "The name is required.",nameof(name));
+        Ensure.NotEmpty(description, "The description is required.",nameof(description));
+        Ensure.NotNull(user, "The user is required.", nameof(user));
+
+        Name = name;
+        Description = description;
+        DeadLine = deadLine;
+        Priority = priority;
+        Order = order;
+        CreatorId = user.Id;
+    }
+
+    public static Task Create(Name name, User user, string description, DateTime deadLine, int priority, int order)
+    {
+        var task =new Task(name, user, description, deadLine, priority, order);
+
+        task.AddDomainEvent(new TaskCreatedDomainEvent(task));
+
+        return task;
+    }
+
+    void Update(Name name, string description, DateTime deadLine, int priority, int order)
+    {
+        Ensure.NotEmpty(name, "The name is required.", nameof(name));
+        Ensure.NotEmpty(description, "The description is required.", nameof(description));
+
+        Name = name;
+        Description = description;
+        DeadLine = deadLine;
+        Priority = priority;
+        Order = order;
+
+        AddDomainEvent(new TaskUpdatedDomainEvent(this));
+    }
+
+    public Name Name { get; private set; }
+    public string Description { get; private set; }
+    public DateTime DeadLine { get; private set; }
+    public int Priority { get; private set; }
+    public int Order { get; private set; }
+    public Guid CreatorId { get;private set; }
+    public DateTime CreatedOnUtc  { get; }
+    public DateTime? ModifiedOnUtc { get; }
+    public DateTime? DeletedOnUtc { get; }
+    public bool Deleted { get; }
+}
