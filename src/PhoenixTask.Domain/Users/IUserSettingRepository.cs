6ea3006e32a11db85de951815251262134b3c84b
Erfan Memarpoor <ErfanMelon@outlook.com>
2024-10-05 11:51:38 +0330

    feat : Add user setting entity

---
+++ src/PhoenixTask.Domain/Users/IUserSettingRepository.cs
@@ -0,0 +1,12 @@
+﻿using PhoenixTask.Domain.Abstractions.Maybe;
+
+namespace PhoenixTask.Domain.Users;
+
+public interface IUserSettingRepository
+{
+    Task<Maybe<UserSetting>> GetUserSettingByKeyAsync(Guid userId, string key);
+    Task<IEnumerable<UserSetting>> GetUserSettingsAsync(Guid userId);
+    void Insert(UserSetting userSetting);
+    void Update(UserSetting userSetting);
+    void Delete(UserSetting userSetting);
+}
---
+++ src/PhoenixTask.Domain/Users/UserSetting.cs
@@ -0,0 +1,24 @@
+﻿#pragma warning disable
+using PhoenixTask.Domain.Abstractions.Guards;
+using PhoenixTask.Domain.Abstractions.Primitives;
+
+namespace PhoenixTask.Domain.Users;
+
+public sealed class UserSetting : Entity
+{
+    private UserSetting() { }
+    private UserSetting(User user, string key, string value)
+        : base(Guid.NewGuid())
+    {
+        Ensure.NotNull(user, "The user is requierd.", nameof(user));
+        Ensure.NotEmpty(key, "The key is requierd.", nameof(key));
+        Ensure.NotEmpty(value, "The value is requierd.", nameof(value));
+
+        UserId = user.Id;
+        Key = key;
+        Value = value;
+    }
+    public Guid UserId { get; private set; }
+    public string Key { get; set; }
+    public string Value { get; set; }
+}
