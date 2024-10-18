be0de0ff1c087259b3c3db7be23570dbd8b0e7f7
ErfanMelon <ErfanMelon@outlook.com>
2024-10-18 11:34:34 +0330

    feat: Add SetSetting command and handler

---
+++ src/PhoenixTask.Application/Users/SetSetting/SetSettingCommand.cs
@@ -0,0 +1,6 @@
+﻿using PhoenixTask.Application.Abstractions.Messaging;
+using PhoenixTask.Domain.Abstractions.Result;
+
+namespace PhoenixTask.Application.Users.SetSetting;
+
+public sealed record SetSettingCommand(string Key, string Value) : ICommand<Result>;
---
+++ src/PhoenixTask.Application/Users/SetSetting/SetSettingCommandHandler.cs
@@ -0,0 +1,56 @@
+﻿using PhoenixTask.Application.Abstractions.Authentication;
+using PhoenixTask.Application.Abstractions.Data;
+using PhoenixTask.Application.Abstractions.Messaging;
+using PhoenixTask.Domain.Abstractions.Result;
+using PhoenixTask.Domain.Errors;
+using PhoenixTask.Domain.Users;
+
+namespace PhoenixTask.Application.Users.SetSetting;
+
+internal sealed class SetSettingCommandHandler
+    (ISettingRepository settingRepository,
+    IUserIdentifierProvider userIdentifierProvider,
+    IUserRepository userRepository,
+    IUnitOfWork unitOfWork)
+    : ICommandHandler<SetSettingCommand, Result>
+{
+    private readonly ISettingRepository _settingRepository = settingRepository;
+    private readonly IUserIdentifierProvider _userIdentifierProvider = userIdentifierProvider;
+    private readonly IUserRepository _userRepository = userRepository;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+    public async Task<Result> Handle(SetSettingCommand request, CancellationToken cancellationToken)
+    {
+        var keyResult = Key.Create(request.Key);
+
+        if (keyResult.IsFailure)
+        {
+            return Result.Failure(keyResult.Error);
+        }
+
+        var maybeUser = await _userRepository.GetByIdAsync(_userIdentifierProvider.UserId);
+
+        if (maybeUser.HasNoValue)
+        {
+            return Result.Failure(DomainErrors.User.NotFound);
+        }
+
+        var maybeSetting = await _settingRepository.GetSettingAsync(maybeUser.Value, keyResult.Value);
+
+        if (maybeSetting.HasNoValue)
+        {
+            var setting = new Setting() { Key = keyResult.Value, Value = request.Value, UserId = maybeUser.Value.Id };
+            _settingRepository.Insert(setting);
+        }
+        else
+        {
+            var setting = maybeSetting.Value;
+            setting.Value = request.Value;
+            _settingRepository.Update(setting);
+        }
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
