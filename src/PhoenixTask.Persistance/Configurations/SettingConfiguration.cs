01ccfd35b300af624eb2ca5cde218649d3e60b4b
ErfanMelon <ErfanMelon@outlook.com>
2024-10-18 11:02:06 +0330

    feat: add Setting to database

---
+++ src/PhoenixTask.Persistance/Configurations/SettingConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PhoenixTask.Domain.Users;
+
+namespace PhoenixTask.Persistance.Configurations;
+
+internal sealed class SettingConfiguration : IEntityTypeConfiguration<Setting>
+{
+    public void Configure(EntityTypeBuilder<Setting> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.OwnsOne(setting => setting.Key, keybuilder =>
+        {
+            keybuilder.WithOwner();
+
+            keybuilder.Property(key => key.Value)
+                .HasColumnName(nameof(Setting.Key))
+                .HasMaxLength(Key.MaxLength)
+                .IsRequired();
+        });
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(x => x.UserId);
+
+        builder.Property(setting => setting.Value)
+            .IsRequired();
+    }
+}
---
+++ src/PhoenixTask.Persistance/Migrations/20241018073024_add-setting.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhoenixTask.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class addsetting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Setting",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Key = table.Column<string>(type: "nvarchar(130)", maxLength: 130, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Setting", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Setting_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Setting_UserId",
+                table: "Setting",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Setting");
+        }
+    }
+}
