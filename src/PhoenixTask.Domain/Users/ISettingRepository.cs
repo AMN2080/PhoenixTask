eebce189980b9c3aeb20706e24e21b7b64bc3c06
ErfanMelon <ErfanMelon@outlook.com>
2024-10-18 11:34:14 +0330

    feat: Add SettingRepository interface

---
+++ src/PhoenixTask.Domain/Users/ISettingRepository.cs
@@ -0,0 +1,11 @@
+﻿using PhoenixTask.Domain.Abstractions.Maybe;
+
+namespace PhoenixTask.Domain.Users;
+
+public interface ISettingRepository
+{
+    void Insert(Setting setting);
+    void Update(Setting setting);
+    Task<IEnumerable<Setting>> GetSettingsAsync(User user);
+    Task<Maybe<Setting>> GetSettingAsync(User user,Key key);
+}
---
+++ src/PhoenixTask.Persistance/Repositories/ISettingRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoenixTask.Application.Abstractions.Data;
+using PhoenixTask.Domain.Abstractions.Maybe;
+using PhoenixTask.Domain.Users;
+
+namespace PhoenixTask.Persistance.Repositories;
+
+internal sealed class SettingRepository(IDbContext dbContext) : GenericRepository<Setting>(dbContext), ISettingRepository
+{
+    public async Task<Maybe<Setting>> GetSettingAsync(User user, Key key) 
+        => Maybe<Setting>.From(await DbContext.Set<Setting>().SingleOrDefaultAsync(c => c.UserId == user.Id && c.Key.Value == key.Value));
+
+    public async Task<IEnumerable<Setting>> GetSettingsAsync(User user) 
+        => await DbContext.Set<Setting>().Where(x => x.UserId == user.Id).ToListAsync();
+}
