c030364d22dbb2f48579f2d9835adccd9a27617b
ErfanMelon <ErfanMelon@outlook.com>
2024-10-18 11:01:48 +0330

    feat: Add UserId to Setting entity

--- src/PhoenixTask.Domain/Users/Setting.cs
+++ src/PhoenixTask.Domain/Users/Setting.cs
@@ -6,4 +6,5 @@ public sealed class Setting : Entity
 {
     public Key Key { get; set; } = Key.Default;
     public string Value { get; set; } = string.Empty;
+    public Guid UserId { get; set; }
 }
