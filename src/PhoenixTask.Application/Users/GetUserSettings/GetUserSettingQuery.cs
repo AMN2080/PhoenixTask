7d2677adc4a87047fb3731d9efcf91faf12542ab
ErfanMelon <ErfanMelon@outlook.com>
2024-10-18 11:36:13 +0330

    feat: Add GetUserSettingQuery and handler

---
+++ src/PhoenixTask.Application/Users/GetUserSettings/GetUserSettingQuery.cs
@@ -0,0 +1,6 @@
+﻿using PhoenixTask.Application.Abstractions.Messaging;
+using PhoenixTask.Contracts.Users;
+
+namespace PhoenixTask.Application.Users.GetUserSettings;
+
+public sealed record GetUserSettingQuery : IQuery<IEnumerable<SettingModel>>;
---
+++ src/PhoenixTask.Application/Users/GetUserSettings/GetUserSettingQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using PhoenixTask.Application.Abstractions.Authentication;
+using PhoenixTask.Application.Abstractions.Messaging;
+using PhoenixTask.Contracts.Users;
+using PhoenixTask.Domain.Users;
+
+namespace PhoenixTask.Application.Users.GetUserSettings;
+
+internal sealed class GetUserSettingQueryHandler(
+    IUserRepository userRepository,
+    IUserIdentifierProvider userIdentifierProvider,
+    ISettingRepository settingRepository)
+    : IQueryHandler<GetUserSettingQuery, IEnumerable<SettingModel>>
+{
+    private readonly IUserRepository _userRepository = userRepository;
+    private readonly IUserIdentifierProvider _userIdentifierProvider = userIdentifierProvider;
+    private readonly ISettingRepository _settingRepository = settingRepository;
+
+    public async Task<IEnumerable<SettingModel>> Handle(GetUserSettingQuery request, CancellationToken cancellationToken)
+    {
+        var maybeUser = await _userRepository.GetByIdAsync(_userIdentifierProvider.UserId);
+        if (maybeUser.HasNoValue)
+        {
+            return [];
+        }
+
+        var settings =await _settingRepository.GetSettingsAsync(maybeUser.Value);
+
+        return settings.Select(x => new SettingModel { Value = x.Value, Key = x.Key.Value }).ToList();
+    }
+}
